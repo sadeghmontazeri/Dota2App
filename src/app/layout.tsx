3803259dbf3feedbc5bde0b7dcd786396483f069
unknown <s.montazeriha@yahoo.com>
2025-07-16 18:03:06 +0330

    Final fixes for Vercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,14 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      {children}
     </ClerkProvider>
   );
 }
